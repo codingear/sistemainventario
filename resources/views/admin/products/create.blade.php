6f05df9c57b52ea3d03998e4c0c44a84ef912e78
Luis Pastén <luispasten.dev@gmail.com>
2019-10-24 13:23:36 -0500

    Selector de imagenes principales y galeria js terminado

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Nuevo Producto')
 @push('stylesheets')
     <link rel="stylesheet" href="{{ asset('vendors/bootstrap-select/bootstrap-select.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/gallery.css') }}">
 @endpush
 @section('content')
       <!-- Page Heading -->
@@ -23,8 +22,8 @@
         @csrf
         @method('POST')
         <div class="row">
-            <div class="col-md-12 col-lg-8">
-                <div class="card shadow mb-4 mt-2 border-bottom-main">
+            <div class="col-md-12 col-lg-9 d-flex align-items-stretch">
+                <div class="card shadow mb-4 mt-2 border-bottom-main w-100">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-main">
                             Datos Producto
@@ -81,12 +80,16 @@
                                 <input type="number" class="form-control" value=""
                                        id="stock" name="stock">
                             </div>
-                            <div class="form-group col-lg-4 col-md-12">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-lg-12 col-md-12">
                                 <label for="description" class="col-form-label">Descripción</label>
-                                <textarea class="form-control" id="description" rows="3"
+                                <textarea class="form-control" id="description" rows="4"
                                           name="description"></textarea>
                             </div>
                         </div>
+                    </div>
+                    <div class="card-footer">
                         <div class="btn-action">
                             <button class="btn btn-success btn-icon-split btn-sm" type="submit" id="send_form">
                             <span class="icon text-white-50">
@@ -104,8 +107,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-12 col-lg-4">
-                <div class="card shadow mb-4 mt-2 border-bottom-main">
+            <div class="col-md-12 col-lg-3 d-flex align-items-stretch">
+                <div class="card shadow mb-4 mt-2 border-bottom-main w-100">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-main">
                             Imagen principal
@@ -113,13 +116,17 @@
                     </div>
                     <div class="card-body mb-2">
                         <p class="text-center align-middle">
-                            <button type="button" class="btn btn-info btn-icon-split btn-sm" data-toggle="modal" data-target=".bd-example-modal-xl">
-                                <span class="icon text-white-50">
-                                    <i class="fas fa-upload fa-sm text-white-50"></i>
-                                </span>
-                                <span class="text text-light">Agregar imagen principal</span>
-                            </button>
+                            <img src="https://dummyimage.com/900x900/f8f9fc/919191.png&text=Sin+Imagen" class="img__principal" alt="" id="principal_image">
+                            <input type="text" class="visible" name="principal_image" id="principal_image_field" >
                         </p>
+                    </div>
+                    <div class="card-footer text-muted text-center">
+                        <button type="button" class="btn btn-info btn-icon-split btn-sm" data-toggle="modal" data-target=".bd-example-modal-xl" data-action="to_principal">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-upload fa-sm text-white-50"></i>
+                            </span>
+                            <span class="text text-light">Agregar</span>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -127,7 +134,7 @@
     </form>
     <div class="row">
         <div class="col-12">
-            <div class="card shadow mb-4 mt-2 border-bottom-main">
+            <div class="card shadow mb-4 mt-2 border-bottom-main text-center">
                 <div class="card-header py-3 d-flex align-items-center">
                     <h6 class="m-0 font-weight-bold text-main">
                         Galería de imágenes
@@ -139,13 +146,18 @@
                 </div>
                 <div class="card-body mb-2">
                     <p class="text-center align-middle">
-                        <a href="#"class="btn btn-info btn-icon-split btn-sm">
-                            <span class="icon text-white-50">
-                                <i class="fas fa-upload fa-sm text-white-50"></i>
-                            </span>
-                            <span class="text text-light">Agregar galería de imagenes</span>
-                        </a>
+                        <div id="gallery_select" class="container__imgs">
+                            <p class="card-text">Aún no se cuentan con imagenes</p>
+                        </div>
                     </p>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-info btn-icon-split btn-sm" data-toggle="modal" data-target=".bd-example-modal-xl" data-action="to_gallery">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-upload fa-sm text-white-50"></i>
+                        </span>
+                        <span class="text text-light">Agregar</span>
+                    </button>
                 </div>
             </div>
         </div>
@@ -170,24 +182,37 @@
                     <div class="tab-content" id="myTabContent">
                         <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="home-tab">
                             <!-- LISTA DE IMAGENES -->
-                            <div class="container__imgs">
-                                <div class="img__item">
-                                    
-                                </div>
-                                <div class="img__item">
-
-                                </div>
-                                <div class="img__item">
-
-                                </div>
-                                <div class="img__item">
-
-                                </div>
-                            </div>                    
+                            <ul id="gallery" class="container__imgs" data-selector="">
+                                <li class="" data-id="001">
+                                    <img src="https://dummyimage.com/100x100/F5B551/fff.png" alt="">
+                                </li>
+                                <li class="" data-id="002">
+                                    <img src="https://dummyimage.com/100x100/6CD7FD/fff.png" alt="">
+                                </li>
+                                <li class="" data-id="003">
+                                    <img src="https://dummyimage.com/100x100/9FA2FB/fff.png" alt="">
+                                </li>
+                                <li class="" data-id="004">
+                                    <img src="https://dummyimage.com/100x100/DAC514/fff.png" alt="">
+                                </li>
+                                <li class="" data-id="005">
+                                    <img src="https://dummyimage.com/100x100/de2fde/fff.png" alt="">
+                                </li>
+                            </ul>          
+                            <style>
+                                .ms-selected{
+                                    display: inline-block;
+                                    border: 2px solid blue;
+                                }
+                                .img__principal{
+                                    width: 100%;
+                                    margin-bottom: 1.5em;
+                                }
+                            </style>          
                             <!-- LISTA DE IMAGENES -->   
                         </div>
                         <div class="tab-pane fade" id="upload" role="tabpanel" aria-labelledby="profile-tab">
-                          
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit non quaerat cumque, veniam odit sint.
                         </div>
                     </div>
                 </div>
@@ -198,7 +223,7 @@
                         </span>
                         <span class="text text-light">Cerrar</span>
                     </button>
-                    <button type="button" class="btn btn-info btn-icon-split btn-sm" data-toggle="modal" data-target=".bd-example-modal-xl" data-dismiss="modal">
+                    <button type="button" id="select_imgs" class="btn btn-info btn-icon-split btn-sm" data-toggle="modal" data-target=".bd-example-modal-xl" data-dismiss="modal">
                         <span class="icon text-white-50">
                             <i class="fas fa-plus fa-sm text-white-50"></i>
                         </span>
@@ -208,14 +233,123 @@
             </div>
         </div>
     </div>
-    <script>
-        $('#myModal').modal('show');
-    </script>
     <!-- VENTANA MODAL -->
 @endsection
 @push('optional_scripts')
     <script src="{{ asset('vendors/bootstrap-select/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('vendors/multiselector/multiselector.min.js') }}"></script>
     <script>
+        // SELECCIONAR IMAGENES DE GALERIA
+        
+        $('#gallery').multiSelector({
+            selector:'li',
+            disableShift: false,
+            disableCtrl: false,
+        });
+        
+   
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#select_imgs").removeAttr("data-section");
+            var $action = e.relatedTarget.dataset.action;
+            if($action == 'to_principal'){
+                $("#gallery").attr("data-selector","to_principal");
+                $("#select_imgs").attr("data-section","principal");
+                // LIMPIANDO EL PROTOTIPO
+                $('#gallery').unbind();
+                // LIMPIANDO EL PROTOTIPO
+                $('#gallery').multiSelector({
+                    selector:'li',
+                    disableShift: true,
+                    disableCtrl: true,
+                });
+                
+            }else if($action == 'to_gallery'){
+                $("#gallery").attr("data-selector","to_gallery");
+                $("#select_imgs").attr("data-section","gallery");
+                // LIMPIANDO EL PROTOTIPO
+                $('#gallery').unbind();
+                // LIMPIANDO EL PROTOTIPO
+                $('#gallery[data-selector="to_gallery"]').multiSelector({
+                    selector:'li',
+                    disableShift: false,
+                    disableCtrl: false,
+                });
+            }
+        });
+        $('#myModal').on('hidden.bs.modal', function (e) {
+            $("#gallery").multiSelector('deselect');
+        })
+
+        var $list_images = [];
+
+        $("#select_imgs").click(function(e){
+            var $section = e.currentTarget.dataset.section;
+            
+            console.log($section);
+
+            var $items = $("#gallery").multiSelector('get');
+            $.each($items, function(k,v){
+                var $id = v.dataset['id'];
+                var $src = v.children[0].attributes.src.nodeValue;
+                if($section == 'principal'){
+                    $("#principal_image").attr("src",$src);
+                    $("#principal_image_field").val($id);
+                }else if($section == 'gallery'){
+                    checkImage($id,$src);
+                }
+            })
+            printImages();
+        });
+
+        $("#gallery_select").on('click','button#deleteImg',function(i){
+            $id = i.target.dataset.id;
+            for (let index = 0; index < $list_images.length; index++) {
+                if($list_images[index].key == $id){
+                    $list_images.splice(index,1);
+                    break;
+                }
+            }            
+            printImages();
+        });
+     
+        function checkImage($key,$src){
+            if($list_images.length > 0){
+                var repetido = false;
+                $.each($list_images,function(k,v){
+                    if(v.key == $key){
+                        repetido = true;
+                    }
+                })
+                if(repetido == false){
+                    $list_images.push({
+                        "key" : $key,
+                        "src" : $src
+                    });
+                }
+            }else{
+                $list_images.push({
+                    "key" : $key,
+                    "src" : $src
+                });
+            }
+        };
+
+        function printImages(){
+            if($list_images.length == 0){
+                $("#gallery_select").html('<p class="card-text">Aún no se cuentan con imagenes</p>');
+            }else{
+                $("#gallery_select").html("");
+                $.each($list_images,function(ke,va){
+                    $("#gallery_select").append("<img src=" + va.src +"><input type=text value="+va.key+"><button id=deleteImg data-id="+va.key+">delete</button>");
+                })
+            }
+        }
+
+        
+        
+        
+        // SELECCIONAR IMAGENES DE GALERIA
+
         $(document).ready(function () {
             $('#category_id').selectpicker({
                 "noneResultsText": 'No hay resultados coincidentes'
@@ -223,12 +357,6 @@
             $(function () {
                 $('[data-toggle="popover"]').popover()
             })
-
-            // if ($(".imgPreview").toArray().length > 4) {
-            //     document.querySelector('.imgAdd').style.setProperty('display', 'none', 'important');
-            // } else if ($(".imgPreview").toArray().length < 5) {
-            //     document.querySelector('.imgAdd').style.setProperty('display', 'flex', 'important');
-            // }
         });
 
         (function () {
