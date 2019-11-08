fecf0d99778a09d0aaf281d9b9298a0a84d509df
Isidro Martinez <isidro.ram.mar@gmail.com>
2019-11-07 19:55:46 -0600

    ignore Categories, Products, Providers Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,10 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(StatesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(ProvidersTableSeeder::class);
-
-
+        // $this->call(CategoriesTableSeeder::class);
+        // $this->call(ProductsTableSeeder::class);
+        // $this->call(ProvidersTableSeeder::class);
     }
 }
